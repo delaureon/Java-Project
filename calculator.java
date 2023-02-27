41b596d899cbb3041491221d50b79e94230fe262
delaureon <arenlizardo020298@gmail.com>
2023-02-27 12:39:24 -0800

    Preparing the functions

--- calculator.java
+++ calculator.java
@@ -1,6 +1,9 @@
 class calculator {
-    
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        add(3,4);
+        sub(4,3);
+        multi(6,6);
+        div(3,3);
     }
 }
